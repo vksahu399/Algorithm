e68d598e39f50c4e980e124de91017012b346187
jrani3677 <92138357+jrani3677@users.noreply.github.com>
2021-10-23 11:18:14 +0530

    Create Simple Array Sum.py

---
+++ Simple Array Sum.py
@@ -0,0 +1,20 @@
+#!/bin/python3
+
+import os
+import sys
+
+def simpleArraySum(ar):
+    return sum(ar)
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    ar_count = int(input())
+
+    ar = list(map(int, input().rstrip().split()))
+
+    result = simpleArraySum(ar)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
